477b667a3229cecda797470ff5b8807fa5a708b8
Mahdi <mahdi.bani.2003@gmail.com>
2022-07-13 16:56:23 +0400

    Removed cmake-build-debug

---
+++ src/windows/Game.cpp
@@ -0,0 +1,19 @@
+#include "Game.h"
+
+Game::Game() : QGraphicsView(){
+    setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+    setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+    showFullScreen();
+    //QPixmap pixmap(":/images/bg1");
+    //pixmap = pixmap.scaled(height(),width(),Qt::AspectRatioMode::IgnoreAspectRatio);
+
+    auto scene = new QGraphicsScene(this);
+    //auto textField = new TextField(150,200);
+    //textField->setPlainText("First Name");
+    scene->setSceneRect(0,0,width(),height());
+    //scene->setBackgroundBrush(QBrush(pixmap));
+    scene->setBackgroundBrush(QBrush("#41E3F0"));
+    setScene(scene);
+
+
+}
---
+++ src/windows/Game.h
@@ -0,0 +1,12 @@
+#ifndef PLAY_WITH_FIRE_GAME_H
+#define PLAY_WITH_FIRE_GAME_H
+
+#include <QGraphicsView>
+
+class Game : public QGraphicsView{
+public:
+    Game();
+};
+
+
+#endif //PLAY_WITH_FIRE_GAME_H
